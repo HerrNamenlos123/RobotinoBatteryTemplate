3a7d9d4feed35bd893aa44d34a0928863332e323
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-10-13 11:50:02 +0200

    Works

--- include/RobotinoLayer.h
+++ include/RobotinoLayer.h
@@ -7,7 +7,7 @@ public:
 
 	// Here you can store data which is specific to the Robotino Layer
 	Robotino::Robotino robotino;
-	std::string hostname = "127.0.0.1";
+	std::string hostname = "172.26.1.1";
 
 	float linearSpeed = 0.3f;
 	float turnSpeed = 0.3f;
@@ -25,6 +25,11 @@ public:
 		float y = (Battery::GetApp().GetKey(ALLEGRO_KEY_LEFT) ? linearSpeed : 0) + (Battery::GetApp().GetKey(ALLEGRO_KEY_RIGHT) ? -linearSpeed : 0);
 		float omega = (Battery::GetApp().GetKey(ALLEGRO_KEY_PGUP) ? turnSpeed : 0) + (Battery::GetApp().GetKey(ALLEGRO_KEY_PGDN) ? -turnSpeed : 0);
 		robotino.DriveDirect(x, y, omega);
+
+		if (robotino.GetBumper()) {
+			robotino.DriveDirect(0, 0, 0);
+			Battery::ShowWarningMessageBox("Aua du Wixa");
+		}
 	}
 
 	void OnRender() override {		// Called every frame after update, render graphics here
